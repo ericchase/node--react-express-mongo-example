19a857d62723ade6d06bca65c7209219d0f58ab2
Chase <>
2022-02-16 16:21:47 -0600

    A: Mongoose to backend

--- back-end/server.js
+++ back-end/server.js
@@ -1,23 +1,48 @@
-let express = require('express');
-let cors = require('cors');
-let app = express();
-app.use(cors());
-let MongoClient = require('mongodb').MongoClient;
-let url = 'mongodb+srv://readonly:48z43UnL5B2j5BWn8nWU2R69Zqk97vTv@cluster0.x1lvu.mongodb.net/test';
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-app.route('/api/home').get(function (req, res) {
+const app = express();
+app.use(cors());
+
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://readonly:48z43UnL5B2j5BWn8nWU2R69Zqk97vTv@cluster0.x1lvu.mongodb.net';
+
+app.route('/mongodb').get(function (req, res) {
     MongoClient.connect(url, async function (err, connection) {
         if (err) throw err;
-        let db = connection.db('sample_training');
-        let collection = db.collection('zips');
-        let cursor = collection.find().limit(1);
+        const db = connection.db('sampledb');
+        const collection = db.collection('collection0');
+        const cursor = collection.find().limit(1);
         cursor.toArray(function (err, data) {
             if (err) throw err;
-            console.log(data);
             res.send(data);
             connection.close();
         });
     });
 });
 
-let server = app.listen(8080, function () { });
+
+mongoose.connect("mongodb+srv://readonly:48z43UnL5B2j5BWn8nWU2R69Zqk97vTv@cluster0.x1lvu.mongodb.net/sampledb", {
+    useNewUrlParser: true
+});
+
+const collection0 = mongoose.model("collection0",
+    new mongoose.Schema({
+        hello: {
+            type: String
+        }
+    })
+);
+
+app.route('/mongoose').get(function (req, res) {
+    collection0.find({}, function (err, result) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+const server = app.listen(8080, function () { });

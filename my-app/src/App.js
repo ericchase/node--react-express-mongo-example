30e6de97718c298fd661d5e30ef6df1678a1c218
Chase <>
2022-02-16 15:48:30 -0600

    A: Pre-built react app directory

---
+++ my-app/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  const [array, setArray] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('http://localhost:8080/api/home', { mode: 'cors' })
+      .then(res => res.json())
+      .then(setArray);
+  }, []);
+
+  return (
+    <div className="App">
+      {<div>
+        {
+          array
+            ? array.map(item => Object.entries(item).map(([key, value]) =>
+              <p key={key}>{key}: {JSON.stringify(value)}</p>
+            ))
+            : "Loading..."
+        }
+      </div>}
+    </div>
+  );
+}
+
+export default App;
